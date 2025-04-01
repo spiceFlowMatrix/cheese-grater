4eb992ee8566d83270e9cb0183610ae9500e6dec
Hamza Yazar <rahimy.hamza@gmail.com>
2025-04-01 21:45:42 +0300

    Add simple material grid that expects header configuration and data to display

--- grid/src/lib/grid/grid.component.ts
+++ grid/src/lib/grid/grid.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+
+export interface GridColumnHeader {
+  /** The name of the property for which this header will be used */
+  value: string;
+  /** This string will be rendered as the column header */
+  label: string;
+}
 
 @Component({
   selector: 'lib-grid',
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './grid.component.html',
   styleUrl: './grid.component.css',
 })
-export class GridComponent {}
+export class GridComponent<T> {
+  @Input() displayedColumns: Array<GridColumnHeader> = [];
+  @Input() dataSource: Array<T> = [];
+
+  protected get _columns(): string[] {
+    return this.displayedColumns.map((column) => column.value);
+  }
+}
