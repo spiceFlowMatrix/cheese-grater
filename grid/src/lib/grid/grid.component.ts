e60a80780472022b34e1404f117613fb93fb8c0f
Hamza Yazar <rahimy.hamza@gmail.com>
2025-04-02 22:51:52 +0300

    enh(Grid): Add sorting to grid

--- grid/src/lib/grid/grid.component.ts
+++ grid/src/lib/grid/grid.component.ts
@@ -1,6 +1,16 @@
-import { Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatSortModule, Sort } from '@angular/material/sort';
+import { MatSort } from '@angular/material/sort';
 
 export interface GridColumnHeader {
   /** The name of the property for which this header will be used */
@@ -11,15 +21,40 @@ export interface GridColumnHeader {
 
 @Component({
   selector: 'lib-grid',
-  imports: [CommonModule, MatTableModule],
+  imports: [CommonModule, MatTableModule, MatSortModule],
   templateUrl: './grid.component.html',
   styleUrl: './grid.component.css',
 })
-export class GridComponent<T> {
+export class GridComponent<T> implements OnInit, AfterViewInit {
+  //#region Inputs
   @Input() displayedColumns: Array<GridColumnHeader> = [];
   @Input() dataSource: Array<T> = [];
+  @Input() sorting = false;
+  //#endregion
+
+  //#region Outputs
+  @Output() sortChange = new EventEmitter<Sort>();
+  //#endregion
+
+  //#region Internal Properties
+  _dataSource: MatTableDataSource<T> | null = null;
+  @ViewChild(MatSort) sort: MatSort | null = null;
+  //#endregion
+
+  //#region Angular Lifecycle Hooks
+  ngOnInit(): void {
+    this._dataSource = new MatTableDataSource(this.dataSource);
+  }
+  ngAfterViewInit() {
+    if (this._dataSource) this._dataSource.sort = this.sort;
+  }
+  //#endregion
 
   protected get _columns(): string[] {
     return this.displayedColumns.map((column) => column.value);
+  }
+
+  handleSortChange(sortState: Sort) {
+    this.sortChange.emit(sortState);
   }
 }
