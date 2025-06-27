819c6ca7adbcf7f60e03b849f7397db31ec99fb5
Hamza Yazar <rahimy.hamza@gmail.com>
2025-06-27 17:23:06 +0300

    chore: Migrate all domain classes for initial clean architecture setup

---
+++ libs/dotnet/domain/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Clean.Domain.Common;
+global using Clean.Domain.Entities;
+global using Clean.Domain.Enums;
+global using Clean.Domain.Events;
+global using Clean.Domain.Exceptions;
+global using Clean.Domain.ValueObjects;
