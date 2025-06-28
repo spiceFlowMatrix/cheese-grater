3477c5b15730e239d8f4dc3bf6ccfcf549f6e16c
Hamza Yazar <rahimy.hamza@gmail.com>
2025-06-28 21:42:45 +0300

    chore: Add classlib for infrastructure layer in dotnet stack and reference dotnet projects in nx workspce

---
+++ libs/dotnet/infrastructure/DependencyInjection.cs
@@ -0,0 +1,46 @@
+﻿using CheeseGrater.Application.Common.Interfaces;
+using CheeseGrater.Domain.Constants;
+using CheeseGrater.Infrastructure.Data;
+using CheeseGrater.Infrastructure.Data.Interceptors;
+using CheeseGrater.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static void AddInfrastructureServices(this IHostApplicationBuilder builder)
+    {
+        var connectionString = builder.Configuration.GetConnectionString("CheeseGraterDb");
+        Guard.Against.Null(connectionString, message: "Connection string 'CheeseGraterDb' not found.");
+
+        builder.Services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        builder.Services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+        builder.Services.AddDbContext<ApplicationDbContext>((sp, options) =>
+        {
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+            options.UseSqlServer(connectionString).AddAsyncSeeding(sp);
+        });
+
+
+        builder.Services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+
+        builder.Services.AddScoped<ApplicationDbContextInitialiser>();
+
+        builder.Services
+            .AddDefaultIdentity<ApplicationUser>()
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        builder.Services.AddSingleton(TimeProvider.System);
+        builder.Services.AddTransient<IIdentityService, IdentityService>();
+
+        builder.Services.AddAuthorization(options =>
+            options.AddPolicy(Policies.CanPurge, policy => policy.RequireRole(Roles.Administrator)));
+    }
+}
---
+++ libs/dotnet/infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CheeseGrater.Infrastructure.Identity;
+
+public class ApplicationUser : IdentityUser
+{
+}
