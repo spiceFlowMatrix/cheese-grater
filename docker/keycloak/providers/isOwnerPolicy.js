8c0a14eab0318d5834ae88d823fa9388d34d7acc
Hamza Yazar <rahimy.hamza@gmail.com>
2025-07-03 22:38:24 +0300

    chore: WIP bugfixing custom owner policy for keycloak

--- docker/keycloak/providers/isOwnerPolicy.js
+++ docker/keycloak/providers/isOwnerPolicy.js
@@ -1,25 +1,36 @@
+var context = $evaluation.getContext();
+var permission = $evaluation.getPermission();
+
+isOwnerPolicy(permission, context);
+
 /**
  * Policy: Is Owner
  * Description: Checks if the requesting user is the owner of the resource.
  * Assumes the resource has an attribute 'owner' containing the user ID.
  */
 function isOwnerPolicy(permission, context) {
+    print("Starting isOwnerPolicy evaluation");
+
     var identity = context.identity;
     var resource = permission.resource;
 
     if (!identity || !resource) {
-        // If identity or resource is missing, policy cannot evaluate
-        return false;
+        print("Missing identity or resource: identity=" + identity + ", resource=" + resource);
+        $evaluation.deny();
+        return;
     }
 
-    var resourceOwnerId = resource.get('owner'); // Get the 'owner' attribute from the resource
-    var requestingUserId = identity.get('id');   // Get the ID of the requesting user
+    var resourceOwnerId = resource.get('owner');
+    var requestingUserId = identity.getId();
+
+    print("Resource Owner ID: " + resourceOwnerId);
+    print("Requesting User ID: " + requestingUserId);
 
     if (resourceOwnerId && requestingUserId && resourceOwnerId === requestingUserId) {
-        $evaluation.grant(); // Grant access if the user is the owner
-        return true;
+        print("User is the owner, granting access");
+        $evaluation.grant();
+    } else {
+        print("User is not the owner or IDs are missing, denying access");
+        $evaluation.deny();
     }
-
-    $evaluation.deny(); // Deny access otherwise
-    return false;
 }
