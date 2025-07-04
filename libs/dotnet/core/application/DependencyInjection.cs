af3dbc3fd9c9cc7f458375cc30d1a642320154bf
Hamza Yazar <rahimy.hamza@gmail.com>
2025-07-04 11:46:24 +0300

    chore: Add logging request preprocessor

--- libs/dotnet/core/application/DependencyInjection.cs
+++ libs/dotnet/core/application/DependencyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using CheeseGrater.Core.Application.Common.Behaviours;
+using MediatR.Pipeline;
 using Microsoft.Extensions.Hosting;
 
 namespace Microsoft.Extensions.DependencyInjection;
@@ -11,6 +12,7 @@ public static class DependencyInjection
         builder.Services.AddMediatR(cfg =>
         {
             cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+            cfg.AddRequestPreProcessor(typeof(IRequestPreProcessor<>), typeof(LoggingBehaviour<>));
             cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
             cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehaviour<,>));
             cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
