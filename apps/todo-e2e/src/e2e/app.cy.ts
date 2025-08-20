1ce62a170b54b9012925a14ba1f212edcdecd408
Hamza Yazar <rahimy.hamza@gmail.com>
2025-08-20 15:41:45 +0300

    chore: fix ci

--- apps/todo-e2e/src/e2e/app.cy.ts
+++ apps/todo-e2e/src/e2e/app.cy.ts
@@ -1,4 +1,4 @@
-import { getGreeting } from '../support/app.po';
+import { getGreeting, getLoginButton } from '../support/app.po';
 
 describe('todo-e2e', () => {
   beforeEach(() => cy.visit('/'));
@@ -8,6 +8,6 @@ describe('todo-e2e', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains(/Welcome/);
+    getLoginButton().contains(/Logins/);
   });
 });
