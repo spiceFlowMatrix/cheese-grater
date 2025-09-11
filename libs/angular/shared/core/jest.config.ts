031f4de4b603d6e463078271fdbea93cdd6e5c99
Hamza Yazar <rahimy.hamza@gmail.com>
2025-09-11 18:03:49 +0300

    feat: Bring jest configs back to working state

--- libs/angular/shared/core/jest.config.ts
+++ libs/angular/shared/core/jest.config.ts
@@ -12,7 +12,7 @@ export default {
       },
     ],
   },
-  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$|@faker-js/faker|keycloak-js)'],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
