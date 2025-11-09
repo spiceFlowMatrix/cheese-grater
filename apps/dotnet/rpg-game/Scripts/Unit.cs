07b7160b528365a239612a50759dff7ea32f3736
Hamza Yazar <rahimy.hamza@gmail.com>
2025-11-09 17:29:43 +0300

    feat: Add named script properties

--- apps/dotnet/rpg-game/Scripts/Unit.cs
+++ apps/dotnet/rpg-game/Scripts/Unit.cs
@@ -1,21 +1,20 @@
-using Godot;
 using System;
+using Godot;
 
 public partial class Unit : Node2D
 {
+	[Export]
 	private Sprite2D _sprite;
+
+	[Export]
 	private Node2D _visuals;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		_sprite = GetNode<Sprite2D>("Visuals/Sprite");
-		_visuals = GetNode<Node2D>("Visuals");
-
 		GD.Print(_sprite.Name);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+	public override void _Process(double delta) { }
 }
